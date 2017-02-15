96030ba099c7cff41e9f681609d15da91b91cb2c
Rick Ucker <rick.ucker@gmail.com>
2017-02-15 14:56:23 -0500

    Compile --no-local bash file for other platform.

--- dotfiles.sh
+++ dotfiles.sh
@@ -5,4 +5,9 @@ DIR="$(dirname $([ -L $0 ] && readlink -f $0 || echo $0))"
 dfm -i "$DIR/src" "$@"
 if [[ $# -eq 0 ]]; then
     dfm -i "$DIR/src" --no-local -o $DIR 1>/dev/null
+    if [[ `uname` == "Darwin" ]]; then
+        dfm -i "$DIR/src" -f bashrc --no-local -o $DIR 1>/dev/null
+    else
+        dfm -i "$DIR/src" -f bash_profile --no-local -o $DIR 1>/dev/null
+    fi
 fi
