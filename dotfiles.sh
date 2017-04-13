af3a21b97404c6a071250bf5f98602b8119ee9b9
Rick Ucker <rick.ucker@gmail.com>
2017-04-12 22:06:44 -0400

    -i switch no longer supported

--- dotfiles.sh
+++ dotfiles.sh
@@ -9,12 +9,12 @@ if [[ -z $DFM_SCRIPT ]]; then
     exit 1
 fi
 
-$DFM_SCRIPT -i "$SCRIPT_DIR/src" "$@"
+$DFM_SCRIPT "$SCRIPT_DIR/src" "$@"
 if [[ $# -eq 0 ]]; then
-    $DFM_SCRIPT -i "$SCRIPT_DIR/src" --no-local -o $SCRIPT_DIR 1>/dev/null
+    $DFM_SCRIPT "$SCRIPT_DIR/src" --no-local -o $SCRIPT_DIR 1>/dev/null
     if [[ $(uname) == "Darwin" ]]; then
-        $DFM_SCRIPT -i "$SCRIPT_DIR/src" -f bashrc --no-local -o $SCRIPT_DIR 1>/dev/null
+        $DFM_SCRIPT "$SCRIPT_DIR/src" -f bashrc --no-local -o $SCRIPT_DIR 1>/dev/null
     else
-        $DFM_SCRIPT -i "$SCRIPT_DIR/src" -f bash_profile --no-local -o $SCRIPT_DIR 1>/dev/null
+        $DFM_SCRIPT "$SCRIPT_DIR/src" -f bash_profile --no-local -o $SCRIPT_DIR 1>/dev/null
     fi
 fi
