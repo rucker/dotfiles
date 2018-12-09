035122ed867cecdcad7c18089126a55aba9c9bfc
Rick Ucker <rick.ucker@gmail.com>
2018-12-09 17:42:47 -0500

    Arg parse handling changes
    - Add -h --help and -i --install opts
    - Validate other args against valid args for dfm.py
    - Add empty _install function

--- dotfiles.sh
+++ dotfiles.sh
@@ -3,9 +3,21 @@
 declare -a ARGS
 declare DOTFILES_SCRIPT_DIR
 NO_PULL=false
+INSTALL=false
+THIS_SCRIPT=$(echo $(basename $([ -L $0 ] && readlink -f $0 || echo $0)))
+DFM_OPTS=($(dfm --help | grep -v -E "\[" | grep -o -- "\s[-]\+[Aa-Zz|-]\+"))
+SCRIPT_OPTS=(-h --help -i --install --no-pull)
+
+__usage() {
+    echo -e "Usage: ${THIS_SCRIPT} ${SCRIPT_OPTS[@]} \n"
+    echo "Dotfiles-Manager (dfm.py) wrapper script"
+}
+
 
 main() {
     _set_args "$@"
+    [[ ${INSTALL} ]] && _install
+
     DOTFILES_SCRIPT_DIR="$(dirname $([ -L $0 ] && readlink -f $0 || echo $0))"
     DFM_SCRIPT=$(readlink ${HOME}/bin/dfm)
     if [[ -z ${DFM_SCRIPT} ]]; then
@@ -17,8 +29,8 @@ main() {
         echo Updating repos...
         repos=( ${DOTFILES_SCRIPT_DIR} $(dirname ${DFM_SCRIPT}) )
         for repo in ${repos[@]}; do
-                _update_repo ${repo}
-                local result=$?
+            _update_repo ${repo}
+            local result=$?
             if [[ ${result} -ne 0 ]]; then
                 echo There was a problem updating repo ${repo}. Abort
                 exit 1
@@ -35,21 +47,36 @@ _set_args() {
     while [[ $# -ge 1 ]]; do
         arg="$1"
         case "${arg}" in
+            -h|--help)
+                __usage
+                exit;;
             --no-pull)
                 NO_PULL=true
                 shift
                 ;;
+            -i|--install)
+                INSTALL=true
+                shift
+                ;;
             *)
-              if ! [[ "$2" =~ - ]]; then
-                  ARGS+=("$1" "$2")
-                  shift
-              else
-                  ARGS+=("$1")
-              fi
-              shift
-              ;;
+                if [[ ! ${DFM_OPTS[@]} =~ ${arg} ]]; then
+                    echo "Unrecognized option ${arg}"
+                    __usage
+                    exit
+                fi
+                if ! [[ "$2" =~ - ]]; then
+                    ARGS+=("$1" "$2")
+                    shift
+                else
+                    ARGS+=("$1")
+                fi
+                shift
+                ;;
         esac
     done
+}
+
+_install() {
 }
 
 _update_repo() {
@@ -57,9 +84,8 @@ _update_repo() {
     echo $1
     if [[ -z $(git status --porcelain) ]]; then
         git pull
-        local this_script=$(echo $(basename $([ -L $0 ] && readlink -f $0 || echo $0)))
         if [[ $1 == ${DOTFILES_SCRIPT_DIR} ]]; then
-            local modified_in_head=$(git diff-tree --no-commit-id --name-only -r HEAD | grep -q ${this_script}; echo $?)
+            local modified_in_head=$(git diff-tree --no-commit-id --name-only -r HEAD | grep -q ${THIS_SCRIPT}; echo $?)
             if [[ ${modified_in_head} -eq 0 ]]; then
                 read -p "It looks like changes to this script were just pulled down. It is recommended that you exit now and re-run the script to pick up any changes. Exit now? [Y/N]: " answer
                 while true; do
