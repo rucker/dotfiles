e43ab3e55a878b43910a604ed7cc695852403826
Rick Ucker <rick.ucker@gmail.com>
2018-12-13 19:20:05 -0500

    Add -i --install flag, validate passed flags
    - When parsing args, validate against this script's flags and dfm.py's flags
    - Clone dotfiles-manager if not present
    - Create ~/bin if not present
    - If not present, symlink ~/bin -> dotfiles/dotfiles.sh ~/bin/dfm -> dotfiles- manager/dotfilesmanager/dfm.py

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,55 +1,122 @@
-#!/bin/bash
+#!/bin/bash -e
 
 declare -a ARGS
 declare DOTFILES_SCRIPT_DIR
 NO_PULL=false
+THIS_SCRIPT=$(echo $(basename $([ -L $0 ] && readlink -f $0 || echo $0)))
+DFM=${HOME}/bin/dfm
+declare DFM_OPTS
+SCRIPT_OPTS=(-h --help -i --install --no-pull)
+
+_usage() {
+    echo -e "Usage: ${THIS_SCRIPT} ${SCRIPT_OPTS[@]} \n"
+    echo "Dotfiles-Manager (dfm.py) wrapper script"
+}
 
 main() {
-    _set_args "$@"
-    DOTFILES_SCRIPT_DIR="$(dirname $([ -L $0 ] && readlink -f $0 || echo $0))"
-    DFM_SCRIPT=$(readlink ${HOME}/bin/dfm)
-    if [[ -z ${DFM_SCRIPT} ]]; then
-        echo dfm not found on '$PATH'.
-        exit 1
+    if [[ $@ =~ -i || $@ =~ --install ]]; then
+      _install
+      exit
     fi
 
+    if [[ ! -e ${DFM} ]]; then
+        echo "dfm not found in ${HOME}/bin. Run with -i | --install to install."
+        _usage
+        exit 1
+    else
+        _get_dfm_opts
+        _set_opts "$@"
+    fi
+
+    DOTFILES_SCRIPT_DIR=$(realpath $(dirname $([ -L $0 ] && readlink -f $0 || echo $0)))
     if [[ $NO_PULL = false ]]; then
         echo Updating repos...
-        repos=( ${DOTFILES_SCRIPT_DIR} $(dirname ${DFM_SCRIPT}) )
+        repos=( ${DOTFILES_SCRIPT_DIR} $(dirname $(readlink ${DFM})) )
         for repo in ${repos[@]}; do
-                _update_repo ${repo}
-                local result=$?
-            if [[ ${result} -ne 0 ]]; then
-                echo There was a problem updating repo ${repo}. Abort
-                exit 1
-            fi
+            _update_repo ${repo}
         done
-        echo Updates complete. Running dfm
-        echo
+        echo -e "Updates complete. Running dfm \n"
     fi
 
     _run_dfm "${ARGS[@]}"
 }
 
-_set_args() {
+_get_dfm_opts() {
+    DFM_OPTS=($(${DFM} --help | grep -v -E "\[" | grep -o -- "\s[-]\+[A-Za-z|-]\+"))
+}
+
+_set_opts() {
     while [[ $# -ge 1 ]]; do
         arg="$1"
         case "${arg}" in
+            -h|--help)
+                _usage
+                exit;;
             --no-pull)
                 NO_PULL=true
                 shift
                 ;;
+            -i|--install)
+                shift
+                ;;
             *)
-              if ! [[ "$2" =~ - ]]; then
-                  ARGS+=("$1" "$2")
-                  shift
-              else
-                  ARGS+=("$1")
-              fi
-              shift
-              ;;
+                if [[ ! ${DFM_OPTS[@]} =~ ${arg} ]]; then
+                    echo "Unrecognized option ${arg}"
+                    _usage
+                    exit
+                fi
+                if [[ -n "$2" && ! "$2" =~ - ]]; then
+                    ARGS+=("$1" "$2")
+                    shift
+                else
+                    ARGS+=("$1")
+                fi
+                shift
+                ;;
         esac
     done
+}
+
+_install() {
+    echo Installing...
+    local repos_dir=$(dirname $(dirname $(realpath BASH_SOURCE[0])))
+
+    if [[ ! -d ${repos_dir} ]]; then
+        echo Creating ${repos_dir}
+        mkdir ${repos_dir}
+    fi
+
+    pushd ${repos_dir} 2>&1 > /dev/null
+
+    if [[ ! -d dotfiles-manager ]]; then
+        echo Cloning Dotfiles Manager
+        git clone https://github.com/rucker/dotfiles-manager.git
+    fi
+
+    local home_bin=${HOME}/bin
+    if [[ ! -d ${home_bin} ]]; then
+        echo Creating ${home_bin}
+        mkdir ${home_bin}
+    fi
+
+    if [[ -z $(readlink ${DFM}) ]]; then
+        local dfm_link_target=${repos_dir}/dotfiles-manager/dotfilesmanager/dfm.py
+        echo "Linking ${DFM} -> ${dfm_link_target}"
+        ln -s ${dfm_link_target} ${DFM}
+    fi
+
+    local dotfiles_link=${home_bin}/dotfiles
+    if [[ -z $(readlink ${dotfiles_link}) ]]; then
+        local dotfiles_link_target=${repos_dir}/dotfiles/${THIS_SCRIPT}
+        echo "Linking ${dotfiles_link} -> ${dotfiles_link_target}"
+        ln -s ${dotfiles_link_target} ${home_bin}/dotfiles
+    fi
+
+    echo Installation complete
+
+    popd 2>&1 > /dev/null
+
+    _get_dfm_opts
 }
 
 _update_repo() {
@@ -57,9 +124,8 @@ _update_repo() {
     echo $1
     if [[ -z $(git status --porcelain) ]]; then
         git pull
-        local this_script=$(echo $(basename $([ -L $0 ] && readlink -f $0 || echo $0)))
         if [[ $1 == ${DOTFILES_SCRIPT_DIR} ]]; then
-            local modified_in_head=$(git diff-tree --no-commit-id --name-only -r HEAD | grep -q ${this_script}; echo $?)
+            local modified_in_head=$(git diff-tree --no-commit-id --name-only -r HEAD | grep -q ${THIS_SCRIPT}; echo $?)
             if [[ ${modified_in_head} -eq 0 ]]; then
                 read -p "It looks like changes to this script were just pulled down. It is recommended that you exit now and re-run the script to pick up any changes. Exit now? [Y/N]: " answer
                 while true; do
@@ -103,7 +169,7 @@ _update_repo() {
 }
 
 _run_dfm() {
-    local DFM_CMD="${DFM_SCRIPT} ${DOTFILES_SCRIPT_DIR}/src"
+    local DFM_CMD="${DFM} ${DOTFILES_SCRIPT_DIR}/src"
     local exclude
     if [[ $(uname) == "Linux" || $(uname) =~ "NT" || ! -d /usr/local/opt/coreutils/libexec/gnubin/ ]]; then
         exclude=98-bashrc_mac
@@ -124,7 +190,7 @@ _run_dfm() {
             let idx++
         done
         local excludes=( -e gitconfig_local -e 98-bashrc_linux -e 98-bashrc_mac -e 97-bashrc_local -o  )
-        ${DFM_SCRIPT} ${DOTFILES_SCRIPT_DIR}/src --no-symlinks ${excludes[@]} ${DOTFILES_SCRIPT_DIR} ${args[@]}
+        ${DFM_CMD} --no-symlinks ${excludes[@]} ${DOTFILES_SCRIPT_DIR} ${args[@]}
 }
 
 main "$@"
